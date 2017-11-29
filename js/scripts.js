60b16e8ed652aa0d11973bedfee3f1d01a3365b6
Natalia Telpukhova and Ross Fletcher <ross@>
2017-11-29 13:28:08 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
+
 var gallonToLiter = function(amount) {
 	return amount / 0.26417;
 }
 
-alert("liters: " + gallonToLiter(prompt("gallons: ")));
+var ouncesToCups = function(amount) {
+	return amount * .125;
+}
+
+var cupsToLiters = function(amount) {
+	return amount * .24;
+}
+$(document).ready(function() {
+	$("#form1").submit(function(event) {
+		var amount = $("input#gal2lit").val();
+		amount = gallonToLiter(amount);
+		$("#output1").text(amount);
+		event.preventDefault();
+	});
+
+	$("#form2").submit(function(event) {
+		var amount = $("input#ounces2cups").val();
+		amount = ouncesToCups(amount);
+		$("#output2").text(amount);
+		event.preventDefault();
+	});
+
+	$("#form3").submit(function(event) {
+		var amount = $("input#cups2liters").val();
+		amount = cupsToLiters(amount);
+		$("#output3").text(amount);
+		event.preventDefault();
+	});
+});
